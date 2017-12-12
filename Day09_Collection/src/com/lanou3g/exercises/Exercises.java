7e42e710697b02e1964ade2de3bfbfa5a00ca4bb
不二_陈 <34052781+NotTwoChen@users.noreply.github.com>
2017-12-12 09:00:28 +0800

    2

---
+++ Day09_Collection/src/com/lanou3g/exercises/Exercises.java
@@ -0,0 +1,31 @@
+package com.lanou3g.exercises;
+
+public class Exercises {
+    public static void main(String[] args) {
+        /**
+         *  第一题:
+         */
+//        // 可以生的兔子
+//        int a = 0;
+//        // 准备生的兔子
+//        int b = 0;
+//        // 不可生的兔子
+//        int c = 1;
+//        // 月份
+//        for (int time = 1; time < 47; time++) {
+//            if (time >1) {
+//                a = a + b;
+//                b = c;
+//                c = a;
+//            }
+//            System.out.println("第"+time+"个月,兔子对数为:"+(a+b+c));
+//        }
+        /**
+         *  第二题:
+         */
+        int a = 0;
+        int b = a * a;
+        
+
+    }
+}
